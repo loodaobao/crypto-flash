798ea6bf752c95061aa373cfe2bfeb6bb1147011
Kobe Wang <gn01703097@gmail.com>
2021-02-16 17:57:30 +0800

    feat(Ws): add web socket client

---
+++ internal/service/exchange/ftx/ws_client.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"log"
+	"net/url"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+const (
+	wsScheme = "wss"
+	wsHost   = "ftx.com"
+	wsPath   = "/ws"
+)
+
+func main() {
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	u := url.URL{Scheme: wsScheme, Host: wsHost, Path: wsPath}
+	log.Printf("connecting to %s", u.String())
+
+	wc, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer wc.Close()
+
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+		for {
+			_, message, err := wc.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				return
+			}
+			log.Printf("recv: %s", message)
+		}
+	}()
+
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-done:
+			return
+		case <-ticker.C:
+			// TODO: Use `WriteJSON`
+			// {"op": "ping"}. You will see an {"type": "pong"}
+			err := wc.WriteMessage(
+				websocket.TextMessage,
+				[]byte(`{"op": "subscribe", "channel": "orderbook", "market": "BTC-PERP"}`),
+			)
+			if err != nil {
+				log.Println("write:", err)
+				return
+			}
+		case <-interrupt:
+			log.Println("interrupt")
+
+			// Cleanly close the connection by sending a close message and then
+			// waiting (with timeout) for the server to close the connection.
+			err := wc.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				log.Println("write close:", err)
+				return
+			}
+			select {
+			case <-done:
+			case <-time.After(time.Second):
+			}
+			return
+		}
+	}
+
+}
