b77b4d327b900af2a5139db7e5fd4318308b3d4a
Kobe Wang <gn01703097@gmail.com>
2021-02-19 16:13:42 +0800

    refactor(Websocket): refactor websocket client

--- internal/service/exchange/ftx/ws_client.go
+++ internal/service/exchange/ftx/ws_client.go
@@ -1,13 +1,17 @@
 package main
 
+// https://github.com/grishinsana/goftx/blob/master/websocket.go
+
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/url"
 	"os"
-	"os/signal"
 	"time"
 
+	"github.com/buger/jsonparser"
 	"github.com/gorilla/websocket"
 )
 
@@ -17,100 +21,255 @@ const (
 	wsPath   = "/ws"
 )
 
-type WS struct {
-	*websocket.Conn
-}
+const (
+	UNDEFINED = iota
+	ERROR
+	TICKER
+	TRADES
+	ORDERBOOK
+	ORDERS
+	FILLS
+)
 
-type PayloadStruct struct {
+type request struct {
 	Op      string `json:"op"`
 	Channel string `json:"channel"`
 	Market  string `json:"market"`
 }
 
-func Connect() *websocket.Conn {
-	u := url.URL{Scheme: wsScheme, Host: wsHost, Path: wsPath}
-	log.Printf("connecting to %s", u.String())
-
-	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
-	if err != nil {
-		log.Fatal("dial:", err)
-	}
-	return c
+type Ticker struct {
+	Bid     float64   `json:"bid"`
+	Ask     float64   `json:"ask"`
+	BidSize float64   `json:"bidSize"`
+	AskSize float64   `json:"askSize"`
+	Last    float64   `json:"last"`
+	Time    time.Time `json:"time"`
 }
 
-func (c *WS) SubscribePairs(channel string, pairs []string) {
-
-	for _, pair := range pairs {
-		fmt.Println("pair: ", pair)
-		payload := &PayloadStruct{
-			Op:      "subscribe",
-			Channel: channel,
-			Market:  pair,
-		}
-
-		c.sendMessage(payload)
-	}
-
+type Trade struct {
+	ID          int       `json:"id"`
+	Liquidation bool      `json:"liquidation"`
+	Price       float64   `json:"price"`
+	Side        string    `json:"side"`
+	Size        float64   `json:"size"`
+	Time        time.Time `json:"time"`
 }
 
-func (c *WS) sendMessage(payload *PayloadStruct) {
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt)
-	done := make(chan struct{})
+type Order struct {
+	ID            int       `json:"id"`
+	ClientID      string    `json:"clientId"`
+	Market        string    `json:"market"`
+	Type          string    `json:"type"`
+	Side          string    `json:"side"`
+	Size          float64   `json:"size"`
+	Price         float64   `json:"price"`
+	ReduceOnly    bool      `json:"reduceOnly"`
+	Ioc           bool      `json:"ioc"`
+	PostOnly      bool      `json:"postOnly"`
+	Status        string    `json:"status"`
+	FilledSize    float64   `json:"filledSize"`
+	RemainingSize float64   `json:"remainingSize"`
+	AvgFillPrice  float64   `json:"avgFillPrice"`
+	CreatedAt     time.Time `json:"createdAt"`
+}
 
-	fmt.Println("payload: ", payload)
+type Response struct {
+	Type   int
+	Symbol string
 
-	go func() {
-		defer close(done)
-		for {
-			_, message, err := c.ReadMessage()
-			if err != nil {
-				log.Println("read:", err)
-				return
+	Ticker    Ticker
+	Trades    Trade
+	Orderbook Orderbook
+	Orders    Order
+
+	Results error
+}
+
+type Orderbook struct {
+	Bids   [][]float64 `json:"bids"`
+	Asks   [][]float64 `json:"asks"`
+	Action string      `json:"action"`
+	// Time     time.Time   `json:"time"`
+	Checksum int `json:"checksum"`
+}
+
+func subscribe(conn *websocket.Conn, channels, symbols []string) error {
+	if symbols != nil {
+		for i := range channels {
+			for j := range symbols {
+				if err := conn.WriteJSON(&request{
+					Op:      "subscribe",
+					Channel: channels[i],
+					Market:  symbols[j],
+				}); err != nil {
+					return err
+				}
 			}
-			log.Printf("recv: %s", message)
 		}
-	}()
+	} else {
+		for i := range channels {
+			if err := conn.WriteJSON(&request{
+				Op:      "subscribe",
+				Channel: channels[i],
+			}); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
 
-	ticker := time.NewTicker(5 * time.Second)
+func ping(conn *websocket.Conn) (err error) {
+	ticker := time.NewTicker(15 * time.Second)
 	defer ticker.Stop()
 
 	for {
 		select {
-		case <-done:
-			return
 		case <-ticker.C:
-			// {"op": "ping"}. You will see an {"type": "pong"}
-			err := c.WriteJSON(payload)
-			if err != nil {
-				log.Println("write:", err)
-				return
+			if err := conn.WriteMessage(websocket.PingMessage, []byte(`{"op": "pong"}`)); err != nil {
+				goto EXIT
 			}
-		case <-interrupt:
-			log.Println("interrupt")
-
-			// Cleanly close the connection by sending a close message and then
-			// waiting (with timeout) for the server to close the connection.
-			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-			if err != nil {
-				log.Println("write close:", err)
-				return
-			}
-			select {
-			case <-done:
-			case <-time.After(time.Second):
-			}
-			return
 		}
 	}
+EXIT:
+	return err
+}
 
+func Connect(ctx context.Context, ch chan Response, channels, symbols []string, l *log.Logger) error {
+	if l == nil {
+		l = log.New(os.Stdout, "ftx websocket", log.Llongfile)
+	}
+
+	u := url.URL{Scheme: wsScheme, Host: wsHost, Path: wsPath}
+	log.Printf("connecting to %s", u.String())
+	conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		return err
+	}
+
+	if err := subscribe(conn, channels, symbols); err != nil {
+		return err
+	}
+
+	// ping each 15sec for exchange
+	go ping(conn)
+
+	go func() {
+		defer conn.Close()
+		// defer unsubscribe(conn, channels, symbols)
+
+	RESTART:
+		for {
+			var res Response
+			_, msg, err := conn.ReadMessage()
+			if err != nil {
+				l.Printf("[ERROR]: msg error: %+v", err)
+				res.Type = ERROR
+				res.Results = fmt.Errorf("%v", err)
+				ch <- res
+				break RESTART
+			}
+
+			typeMsg, err := jsonparser.GetString(msg, "type")
+			if typeMsg == "error" {
+				l.Printf("[ERROR]: error: %+v", string(msg))
+				res.Type = ERROR
+				res.Results = fmt.Errorf("%v", string(msg))
+				ch <- res
+				break RESTART
+			}
+
+			channel, err := jsonparser.GetString(msg, "channel")
+			if err != nil {
+				l.Printf("[ERROR]: channel error: %+v", string(msg))
+				res.Type = ERROR
+				res.Results = fmt.Errorf("%v", string(msg))
+				ch <- res
+				break RESTART
+			}
+
+			market, err := jsonparser.GetString(msg, "market")
+			if err != nil {
+				l.Printf("[ERROR]: market err: %+v", string(msg))
+				res.Type = ERROR
+				res.Results = fmt.Errorf("%v", string(msg))
+				ch <- res
+				break RESTART
+			}
+
+			res.Symbol = market
+
+			data, _, _, err := jsonparser.Get(msg, "data")
+			if err != nil {
+				if isSubscribe, _ := jsonparser.GetString(msg, "type"); isSubscribe == "subscribed" {
+					l.Printf("[SUCCESS]: %s %+v", isSubscribe, string(msg))
+					continue
+				} else {
+					err = fmt.Errorf("[ERROR]: data err: %v %s", err, string(msg))
+					l.Println(err)
+					res.Type = ERROR
+					res.Results = err
+					ch <- res
+					break RESTART
+				}
+			}
+
+			switch channel {
+			case "ticker":
+				res.Type = TICKER
+				if err := json.Unmarshal(data, &res.Ticker); err != nil {
+					l.Printf("[WARN]: cant unmarshal ticker %+v", err)
+					continue
+				}
+
+			case "trades":
+				res.Type = TRADES
+				if err := json.Unmarshal(data, &res.Trades); err != nil {
+					l.Printf("[WARN]: cant unmarshal trades %+v", err)
+					continue
+				}
+
+			case "orderbook":
+				res.Type = ORDERBOOK
+				if err := json.Unmarshal(data, &res.Orderbook); err != nil {
+					l.Printf("[WARN]: cant unmarshal orderbook %+v", err)
+					continue
+				}
+
+			default:
+				res.Type = UNDEFINED
+				res.Results = fmt.Errorf("%v", string(msg))
+			}
+
+			ch <- res
+
+		}
+	}()
+
+	return nil
 }
 
 func main() {
-	c := Connect()
-	defer c.Close()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-	ws := &WS{c}
 	pairs := []string{"BTC-PERP", "ETH-PERP"}
-	ws.SubscribePairs("orderbook", pairs)
+	ch := make(chan Response)
+	go Connect(ctx, ch, []string{"orderbook"}, pairs, nil)
+
+	for {
+		select {
+		case v := <-ch:
+			switch v.Type {
+			case TICKER:
+				fmt.Printf("%s	%+v\n", v.Symbol, v.Ticker)
+
+			case ORDERBOOK:
+				fmt.Printf("%s	%+v\n", v.Symbol, v.Orderbook)
+
+			case UNDEFINED:
+				fmt.Printf("%s	%s\n", v.Symbol, v.Results.Error())
+			}
+		}
+	}
 }
