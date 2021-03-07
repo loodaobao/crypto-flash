7e206ca5e8627ff647811f39be153ea0f99529ae
Kobe Wang <gn01703097@gmail.com>
2021-03-07 08:03:58 +0800

    test(Orderbook): add more orderbook merge unit test for without same price

--- internal/service/util/orderbook_test.go
+++ internal/service/util/orderbook_test.go
@@ -81,6 +81,30 @@ func TestMergeWhenBids_WithZeroCase(t *testing.T) {
 	})
 }
 
+func TestMergeWhenBids_WithoutSamePrice(t *testing.T) {
+	new := [][]float64{
+		{49002, 0.82},
+		{49000, 0.24},
+	}
+
+	expectation := []Row{
+		{49017, 0.3089},
+		{49016, 0.3831},
+		{49014, 18.35},
+		{49008, 0.0547},
+		{49007, 0.139},
+		{49006, 0.307},
+		{49002, 0.82},
+		{49000, 0.24},
+	}
+
+	t.Run("it should be successfully merged", func(t *testing.T) {
+		if got := *Merge(originalForBids, new, "bids"); !reflect.DeepEqual(got, expectation) {
+			t.Errorf("Merge() = %v, want %v", got, expectation)
+		}
+	})
+}
+
 func TestMergeWhenAsks(t *testing.T) {
 	new := [][]float64{
 		{49018, 1.8},
@@ -129,6 +153,33 @@ func TestMergeWhenAsks_WithZeroCase(t *testing.T) {
 		{49042, 36.2009},
 		{49043, 0.8139},
 		{49081, 0.001},
+	}
+
+	t.Run("it should be successfully merged", func(t *testing.T) {
+		if got := *Merge(originalForAsks, new, "asks"); !reflect.DeepEqual(got, expectation) {
+			t.Errorf("Merge() = %v, want %v", got, expectation)
+		}
+	})
+}
+
+func TestMergeWhenAsks_WithoutSamePrice(t *testing.T) {
+	new := [][]float64{
+		{49015, 0.08},
+		{49017, 0.001},
+	}
+
+	expectation := []Row{
+		{49015, 0.08},
+		{49017, 0.001},
+		{49018, 1.3912},
+		{49034, 0.2137},
+		{49036, 0.1836},
+		{49037, 1.0918},
+		{49038, 2.0355},
+		{49040, 0.6653},
+		{49041, 0.82},
+		{49042, 36.2009},
+		{49043, 0.8139},
 	}
 
 	t.Run("it should be successfully merged", func(t *testing.T) {
